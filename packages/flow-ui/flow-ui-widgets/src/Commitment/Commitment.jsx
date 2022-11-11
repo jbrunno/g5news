a3029085d3657f3211b975f2606c6854e5726a1b
Jefferson Bruno Venero da Silva <j.bruno@fisica.ufmt.br>
2022-11-10 22:41:20 -0300

    ajsute plau radio

--- packages/flow-ui/flow-ui-widgets/src/Commitment/Commitment.jsx
+++ packages/flow-ui/flow-ui-widgets/src/Commitment/Commitment.jsx
@@ -8,7 +8,13 @@ const Commitment = props => (
       <Text variant='p'>
         Atuar na defesa da democracia representativa, da livre iniciativa, da liberdade de expressão, da transparência nos gastos públicos e dos interesses dos cidadãos na construção de um país mais justo nas oportunidades...   
       </Text>
-      <iframe src="http://158.69.35.252/public/g5_news/embed?theme=light" frameborder="0" allowtransparency="true" style="width: 100%; min-height: 150px; border: 0;"></iframe>
+      <audio
+        controls
+        src="http://158.69.35.252:8000/radio.mp3">
+            <a href="http://158.69.35.252:8000/radio.mp3">
+                Download audio
+            </a>
+    </audio>
     </Card>
   </Section>
 )
